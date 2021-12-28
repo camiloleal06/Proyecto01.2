2aff08448eeffaafac11a70a31beac0d8d597066
SYSRED PAGOS <gashbell@hotmail.es>
2021-12-28 14:18:42 -0500

    Relacion Many to one

--- src/main/java/com/sysred/controller/PlanController.java
+++ src/main/java/com/sysred/controller/PlanController.java
@@ -18,18 +18,18 @@ import com.sysred.model.Plan;
 
 @Controller
 public class PlanController {
-	
+
 	@Autowired
 	private PlanService planService;
 
-	@RequestMapping(value = {"/","/listarplanes"})
+	@RequestMapping(value = { "/", "/listarplanes" })
 	public String listarPlanes(Model modelo) {
 		List<Plan> plan = planService.findAll();
 		modelo.addAttribute("listaplan", plan);
 		modelo.addAttribute("titulo", "Listado Planes");
 		return "listaPlan";
 	}
-	
+
 	@GetMapping("/formplan")
 	public String crearPlan(Model modelo) {
 		Plan plan = new Plan();
@@ -37,33 +37,27 @@ public class PlanController {
 		modelo.addAttribute("tituloformularioplan", "Nuevo Plan");
 		return "formPlan";
 	}
-	
+
 	@RequestMapping(value = "/editarplan")
 	public String editarPlan(@RequestParam(name = "id") Long id, Model modelo) {
 		Plan plan = planService.findOne(id);
 		modelo.addAttribute("plan", plan);
 		modelo.addAttribute("tituloformularioplan", "Modificar Plan");
-	    return "formPlan";
+		return "formPlan";
 	}
-	
-	@PostMapping("/saveplan")
-	public String SavePlan(Plan plan, Model modelo,RedirectAttributes flash) {
-		planService.save(plan);
-		flash
-		.addFlashAttribute("success", "Plan creado correctamente")
-        .addFlashAttribute("clase", "success");
 
+	@PostMapping("/saveplan")
+	public String SavePlan(Plan plan, Model modelo, RedirectAttributes flash) {
+		planService.save(plan);
+		flash.addFlashAttribute("success", "Plan creado correctamente").addFlashAttribute("clase", "success");
 		return "redirect:/listarplanes";
 	}
 
-	 @GetMapping("/eliminarplan/{id}")
-	   public String eliminarPlan(@PathVariable Long id,
-			   SessionStatus status, 
-			   Model modelo) {
-		   planService.delete(id);
-			status.setComplete();
-			   return "redirect:/listarplanes";
-		}
-	
-	
+	@GetMapping("/eliminarplan/{id}")
+	public String eliminarPlan(@PathVariable Long id, SessionStatus status, Model modelo) {
+		planService.delete(id);
+		status.setComplete();
+		return "redirect:/listarplanes";
+	}
+
 }
